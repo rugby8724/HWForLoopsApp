1222799930a4f07f753e660f919ea85b148713e9
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-07-27 13:55:32 -0500

    Add project files.

---
+++ HWForLoops/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("Please provide a list of names seperated by commas and no spaces between the names");
+string data  = Console.ReadLine();
+
+string[] names = data.Split(',');
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Welcome {names[i]}");
+}
